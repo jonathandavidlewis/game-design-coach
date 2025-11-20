2aaef93e9570dc031a3aa0989ea20827467fce3d
anticdope <pruettdillan@gmail.com>
2025-11-20 14:19:05 -0600

    initial commit with completed task
    completed task to add a email address input

---
+++ components/pages/hero.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link"
+import Image from "next/image"
+import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { heroHeader } from "@/config/contents"  
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export default function HeroHeader() {
+  return (
+    <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-8 lg:py-20">
+      <div className="flex flex-1 flex-col items-center gap-4 text-center lg:gap-8">
+        <div className="space-y-4">
+          <h1 className="text-4xl font-bold lg:text-6xl">
+            {heroHeader.header}
+          </h1>
+          <h2 className="text-lg font-light text-muted-foreground lg:text-3xl">
+            {heroHeader.subheader}
+          </h2>
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-3">
+          <Input type="email" id="email" placeholder="Email" />
+        </div>
+        <Link
+          href=""
+          target="_blank"
+          className={`w-[10rem] ${cn(buttonVariants({ size: "lg" }))}`}
+        >
+          Submit
+        </Link>
+      </div>
+      {heroHeader.image !== "" ? (
+        <div className="flex flex-1 justify-center lg:justify-end">
+          <Image
+            src={heroHeader.image}
+            width={500}
+            height={500}
+            alt="Header image"
+          />
+        </div>
+      ) : (
+        <></>
+      )}
+    </section>
+  )
+}
